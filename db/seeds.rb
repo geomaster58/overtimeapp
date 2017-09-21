2e95b2d156b27345d1541a8369705a53a54f80a2
JJ Keeney <jjkeeney129285@gmail.com>
2017-09-21 10:52:19 -0600

    added bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@ Post.create!(date: Date.today, rationale: "#{post} rational")
 end
 
 puts "100 posts have been created"
+
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-      @post = Post.create(date: Date.today, rationale: "Anything")
+      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+      @post = Post.create(date: Date.today, rationale: "Anything", user_id: @user.id)
     end
     it 'can be created' do
       expect(@post).to be_valid
@@ -11,6 +12,7 @@ RSpec.describe Post, type: :model do
     it "cannot be created without a date and a rationale" do
       @post.date = nil
       @post.rationale = nil
+      @post.user_id = nil
       expect(@post).to_not be_valid
     end
   end
