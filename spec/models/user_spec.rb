fc064e30434aa45b20806c6a8cd5f3dec6c6e206
JJ Keeney <jjkeeney129285@gmail.com>
2017-09-21 14:35:01 -0600

    refactored stuff table to a partial

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "creation" do
-    before do
+
+  before do
      @user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "JJ", last_name: "keeney" )
     end
+  describe "creation" do
+    
     it "can be created" do
       expect(@user).to be_valid
     end
@@ -13,6 +15,12 @@ RSpec.describe User, type: :model do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe "custom name method" do
+    it "has a full name method combining first and last name" do
+      expect(@user.full_name).to eq("KEENEY, JJ")
     end
   end
 end
