18c326a16c69abf231971482b5db7c1439ae3f7a
JJ Keeney <jjkeeney129285@gmail.com>
2017-09-27 14:18:33 -0600

    added overtimehours request to form and model

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ puts "1 user created"
 
 100.times do |post|
 
-Post.create!(date: Date.today, rationale: "#{post+1} rational", user_id: @user.id)
+Post.create!(date: Date.today, rationale: "#{post+1} rational", user_id: @user.id, overtime_request: 2.5)
 
 end
 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,15 +4,20 @@ RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
       @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-      @post = Post.create(date: Date.today, rationale: "Anything", user_id: @user.id)
+      @post = Post.create(date: Date.today, rationale: "Anything", user_id: @user.id, overtime_request: 2.5)
     end
     it 'can be created' do
       expect(@post).to be_valid
     end
-    it "cannot be created without a date and a rationale" do
+    it "cannot be created without a date and a rationale and overtime" do
       @post.date = nil
       @post.rationale = nil
       @post.user_id = nil
+      @post.overtime_request = nil
+      expect(@post).to_not be_valid
+    end
+    it 'has an overtime_request greater than 0.0' do
+      @post.overtime_request = 0.0
       expect(@post).to_not be_valid
     end
   end
