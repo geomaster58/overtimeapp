5f36a512afdaba84013d64fbf87c34e54945cf43
JJ Keeney <jjkeeney129285@gmail.com>
2017-10-10 13:45:07 -0600

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,12 @@ Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside c
 
 puts "1 user created"
 
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+  
+puts "3 AuditLogs created"
+
 100.times do |post|
 
 Post.create!(date: Date.today, rationale: "#{post+1} #{post_text.sample}", user_id: @user.id, overtime_request: 2.5)
@@ -19,8 +25,3 @@ puts "100 posts have been created"
 
 admin = AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "George", last_name: "Snow", phone: "3854397230")
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
-  
-puts "3 AuditLogs created"
